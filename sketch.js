5ffad6ead658822e7a1cd7a3f4738f2dddeca73a
ParamJesani <102550336+ParamJesani@users.noreply.github.com>
2022-03-28 19:54:49 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var box;
+
+
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30);
+}
+
+function draw() 
+{
+  background(212);
+if(keyIsDown(RIGHT_ARROW))
+
+{
+  box . position . x = box . position . x + 5;
+}
+
+if(keyIsDown(LEFT_ARROW))
+
+{
+  box . position . x = box . position . x - 5;
+}
+
+drawSprites();
+
+}
+
+
+
+
